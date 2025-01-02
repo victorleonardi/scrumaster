a771e6202d8d0e1c90e926aee3d042f365b0655d
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-01-02 01:17:08 -0300

    Create Under construction page

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <img style="width: 100px;" src="../public/construction-and-tools.png" alt="Capacete de construção">
+    <h2>Site em construção...</h2>
+    <p>Estamos trabalhando o mais rápido possível para disponibilizar este serviço.</p>
+  </div>
+</template>
