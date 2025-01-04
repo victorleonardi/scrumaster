22822bca250846a511c473db47cbd0bf51281a35
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-01-04 11:28:10 -0300

    Create prisma singleton
    To ensure only one connection is made

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
-
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-prisma = global.prisma;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
