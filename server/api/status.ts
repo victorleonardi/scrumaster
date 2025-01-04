0f96581b375961e39d2a63a369979c6ee5fc3d10
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-01-04 11:49:35 -0300

    test connection with postgres using prisma orm

--- server/api/status.ts
+++ server/api/status.ts
@@ -1,6 +1,10 @@
+import prisma from "~/prisma/prisma"
+
 export default defineEventHandler(async (event) => {
 
+  // Improve postgres connection status check
+  const dbUp = await prisma.$queryRaw`SELECT 1`
   return {
-    postgres: 'ok'
+    postgres: dbUp ? "Ok" : "Error"
   }
 })
