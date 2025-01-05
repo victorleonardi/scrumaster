73aedddbccfb96d416c9ae9b361a2205c5e71f4d
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-01-05 11:02:23 -0300

    Remove previous router structure
    Couldnt make it work with queryparams, don't know why.

--- server/api/[...].ts
+++
@@ -1,7 +0,0 @@
-import userEndpoint from './user/[...].ts'
-
-const router = createRouter()
-
-router.use("/user", userEndpoint)
-
-export default useBase("/api/v1", router.handler)
--- server/api/user/[...].ts
+++
@@ -1,7 +0,0 @@
-import { postUser } from "./create"
-
-const router = createRouter()
-
-router.post("/", defineEventHandler(postUser))
-
-export default useBase("/user", router.handler)
