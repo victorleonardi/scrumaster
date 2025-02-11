188e2c928652243afd1161099495cd647e14d1a3
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-02-10 23:55:37 -0300

    Create basic PageHeader component

---
+++ components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <img class="logo" src="../assets/tempLogo.png" alt="logo">
+    <div class="central-menu">
+      <h2>
+        Menu central em construção
+      </h2>
+    </div>
+    <div class="login-container">
+      <NButton quaternary type="primary" color="#EA193D">Login</NButton>
+      <button>Sign in</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* padding: 0.5rem 1rem; */
+}
+
+.logo {
+  width: 50px;
+  height: 50px;
+}
+
+.login-container {
+  display: flex;
+  gap: 1rem;
+}
+</style>
