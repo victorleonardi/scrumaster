eb6840a7470e3eeb0c8c15c2ecf678f20dc4f510
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-02-10 23:55:50 -0300

    Create route to test

---
+++ pages/test/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <PageHeader/>
+ <div>
+   <h1>
+     Está é uma página de testes apenas
+    </h1>
+  </div>
+  <div class="vote-card">
+    <VoteCard :shouldShow="true" value="10"/>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.vote-card {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
