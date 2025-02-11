a5c3ec064c77202d72c864a2e7fae3a35a90140f
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-02-10 23:54:49 -0300

    Create basic VoteCard component

---
+++ components/VoteCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card" :class="{ 'has_value': hasValue }">
+    <p v-if="shouldShow">
+      {{ value }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed, } from 'vue';
+
+const props = defineProps<{
+  value: string
+  shouldShow: boolean
+}>()
+
+console.log(props)
+const hasValue = computed(() => props.value !== undefined)
+
+console.log(hasValue)
+
+</script>
+
+<style>
+.card {
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background: blue;
+  width: 40%;
+}
+
+.has_value {
+  background: red;
+}
+</style>
