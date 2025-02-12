cc856030b15ff44fbf85715decc3916880b8a140
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-02-12 00:10:46 -0300

    Rearrenge front if few things, adding VoteBar as well
    Still needs to be fixed to place VoteBar where it belongs

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,23 +1,69 @@
 <template>
- <PageHeader/>
- <div>
-   <h1>
-     Está é uma página de testes apenas
-    </h1>
+  <PageHeader />
+  <div class="container">
+    <div class="vote-section-container">
+      <h1>
+        Está é uma página de testes apenas
+      </h1>
+      <div class="center-cards-display">
+        <div class="vote-card-grid">
+          <VoteCard :shouldShow="true" value="10" />
+          <VoteCard :shouldShow="true" value="9" />
+          <VoteCard :shouldShow="true" value="2" />
+          <VoteCard :shouldShow="true" value="11" />
+          <VoteCard :shouldShow="true" value="21" />
+        </div>
+      </div>
+      <NButton type="primary" color="#000000" text-color="#FFFFFF">Ready</NButton>
+    </div>
+    <!-- Jogar o VoteBar para direita independente do espaço. Pensar em como fazer -->
+    <VoteBar class="vote-bar" />
   </div>
-  <div class="vote-card">
-    <VoteCard :shouldShow="true" value="10"/>
 
-  </div>
+
 </template>
 
+
 <script setup lang="ts">
+import { NButton } from 'naive-ui'
 </script>
 
 <style>
-.vote-card {
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+.vote-section-container {
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  gap: 1rem;
+}
+
+.center-cards-display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.vote-card-grid {
+  display: grid;
+  grid-template-rows: repeat(2, auto);
+  grid-auto-flow: column;
+  gap: 10px;
+}
+
+.ready-button {
+  margin-top: 1rem;
+}
+
+.vote-bar {
+  display: flex;
+  justify-content: end;
 }
 </style>
