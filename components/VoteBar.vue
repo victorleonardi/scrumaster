30904feb0ebe75154e4b7adb4d336fbcd0ea05a6
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-02-12 00:10:15 -0300

    Create VoteBar Component testing new element

---
+++ components/VoteBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <NFlex align="center" justify="center" size="small" :vertical="true">
+    <NFloatButton shape="square" height="60" width="60" position="relative" >
+      Teste
+    </NFloatButton>
+    <NFloatButton shape="square" height="60" width="60" position="relative">Peste</NFloatButton>
+    <NFloatButton shape="square" height="60" width="60" position="relative">
+      <NIcon><Balloon /></NIcon>
+    </NFloatButton>
+    <NFloatButton shape="square" height="60" width="60" position="relative">
+      Teste
+    </NFloatButton>
+    <NFloatButton shape="square" height="60" width="60" position="relative">Peste</NFloatButton>
+    <NFloatButton shape="square" height="60" width="60" position="relative">
+      <NIcon><Balloon /></NIcon>
+    </NFloatButton>
+  </NFlex>
+</template>
+
+<script setup lang="ts">
+import { NFloatButton, NIcon, NFlex } from 'naive-ui';
+import { Balloon } from '@vicons/ionicons5';
+</script>
+
+<style>
+.n-float-button {
+  font-size: small;
+}
+</style>
