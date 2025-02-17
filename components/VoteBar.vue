94df99285f40068a66b28a5b3ddcdf7bdb51eb2e
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-02-16 23:29:47 -0300

    Define emit to emit value

--- components/VoteBar.vue
+++ components/VoteBar.vue
@@ -1,6 +1,7 @@
 <template>
   <NFlex align="center" justify="center" size="small" :vertical="true">
-    <NFloatButton shape="square" height="60" width="60" position="relative" v-for="value in values" :key="value">
+    <NFloatButton @click="setCardValue(value)" shape="square" height="60" width="60" position="relative"
+      v-for="value in values" :key="value">
       {{ value }}
     </NFloatButton>
   </NFlex>
@@ -9,10 +10,22 @@
 <script setup lang="ts">
 import { NFloatButton, NIcon, NFlex } from 'naive-ui';
 import { Balloon } from '@vicons/ionicons5';
+import { defineEmits } from "vue";
+
+const emit = defineEmits(['cardValue'])
 
 // Make values reactive due to users choice of values (fibonnaci, custom and others)
 // We can store them in a store like pinia
 const values = [1, 2, 3, 5, 8, 13, 21]
+
+function setCardValue(value: number) {
+  console.log(value)
+
+  emit("cardValue", value)
+}
+
+
+
 </script>
 
 <style>
