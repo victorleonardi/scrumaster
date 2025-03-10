798115919d1457be5b1dca519ac69225e607290c
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-03-10 00:27:49 -0300

    Define server websocket side

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      socketPort: 3001,
+      url: 'http://localhost'
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxtjs-naive-ui']
+  modules: ['nuxtjs-naive-ui'],
+  plugins: ['~/plugins/socket.io.client'],
+  nitro: {
+    plugins: [
+      './plugins/socket.io.server'
+    ]
+  }
 })
