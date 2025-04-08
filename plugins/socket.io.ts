3951db94beb26b580ac56aaddd73934935ee8c00
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-04-07 23:36:50 -0300

    Add event listeners on socket client side

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -7,6 +7,24 @@ export default defineNuxtPlugin(() => {
     autoConnect: false
   })
 
+  socket.on("connect", () => {
+    console.log("✅ Connected to socket.io server!");
+  });
+
+  socket.on("connect_error", (err) => {
+    console.error("❌ Connection error:", err);
+  });
+
+  const websiteStore = useWebsiteStore()
+  //consider changint SocketEvent name
+  socket.on(SocketEvent.new_vote, (message: { sectionId: string, userToken: string, voteValue: number }) => {
+    console.log('new_vote', message)
+    const { sectionId, userToken, voteValue } = message
+    websiteStore.addOrUpdateSection(sectionId, userToken, voteValue)
+  })
+
+
+
   return {
     provide: {
       io: socket
