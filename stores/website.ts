d88850061732d447908d8c9312efd5854e3d1ec8
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-04-07 23:03:18 -0300

    Create storage

--- stores/website.ts
+++ stores/website.ts
@@ -1,10 +1,11 @@
 export const useWebsiteStore = defineStore('websiteStore', {
   state: () => ({
-    sections: {}
+    sections: {} as Record<string, Record<string, number>>
   }),
   actions: {
     addOrUpdateSection(sectionId: string, userToken: string, voteValue: number) {
-      this.sections[section.id][userToken] = voteValue,
+      this.sections[sectionId] ??= {}; // check if the section exists, if not create it
+      this.sections[sectionId][userToken] = voteValue;
     },
     removeSection(sectionId: string) {
       delete this.sections[sectionId]
