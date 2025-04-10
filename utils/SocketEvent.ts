76a8d3f21277a8831588164c9abae38a2a23b98d
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-10 00:30:34 -0300

    Add new socketEvent

--- utils/SocketEvent.ts
+++ utils/SocketEvent.ts
@@ -2,4 +2,5 @@ export enum SocketEvent {
   isReady = 'isReady',
   newVote = 'newVote',
   joinProject = 'joinProject', // would be the same as joinRoom. Later, create an event to leaveRoom
+  updateUsers = 'updateUsers',
 }
