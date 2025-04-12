f597ba8edcb50fefd36aa599d1cbfd890419dc20
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-12 01:31:42 -0300

    Map new event

--- utils/SocketEvent.ts
+++ utils/SocketEvent.ts
@@ -2,5 +2,6 @@ export enum SocketEvent {
   isReady = 'isReady',
   newVote = 'newVote',
   joinProject = 'joinProject', // would be the same as joinRoom. Later, create an event to leaveRoom
+  leaveProject = 'leaveProject',
   updateUsers = 'updateUsers',
 }
