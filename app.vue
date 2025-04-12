3fc1afbe5743912335879539cc420ae2e30d8eb8
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-12 18:40:13 -0300

    Apply notification to all page

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtPage />
+  <NNotificationProvider>
+    <NuxtPage />
+  </NNotificationProvider>
 </template>
+
+<script setup lang="ts">
+import { NNotificationProvider } from 'naive-ui'
+</script>
