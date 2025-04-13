b1b5ab210c715433ab0334d6e8334be711aab192
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-13 00:55:10 -0300

    Change event name

--- utils/SocketEvent.ts
+++ utils/SocketEvent.ts
@@ -2,11 +2,11 @@ export enum SocketEvent {
   isReady = 'isReady',
   allReady = 'allReady',
 
-  newVote = 'newVote',
-
   joinProject = 'joinProject',
   leaveProject = 'leaveProject',
 
   newUser = 'newUser',
   updateUsersInRoom = 'updateUsersInRoom',
+
+  vote = 'vote',
 }
