fb6eea62be5521754846c38746aad0d18eca6d80
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-13 00:54:45 -0300

    Remove useless code

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -14,24 +14,7 @@ export default defineNuxtPlugin(() => {
   socket.on("connect_error", (err) => {
     console.error("❌ Connection error:", err);
   });
-
-  const websiteStore = useWebsiteStore()
-
-  socket.on(SocketEvent.newVote, (message: { userToken: string, isReady: boolean }) => {
-    console.log('newVote', message)
-    const { userToken, isReady } = message
-    if (isReady) websiteStore.newReady(userToken)
-    else websiteStore.waitAMinute(userToken)
-  })
-
-  socket.on(SocketEvent.updateUsers, (message: { userToken: string }) => {
-    console.log('updateUsers', message)
-    const { userToken } = message
-    if (!websiteStore.users.includes(userToken)) {
-      websiteStore.addUser(userToken)
-    }
-  })
-
+  
   return {
     provide: {
       io: socket
