7c911f68adae4dc8f28b518c76b499a55019494e
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-05-17 21:49:06 -0300

    Add a little funny direction to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <template>
   Está é a tela home
+  Acesse `test/[projectId]` para ver os testes de votação
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style>
-
-</style>
+<style></style>
