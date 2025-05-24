377667322e9af3bb6f85e4a7505e76a0a8c3c894
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-05-24 19:23:24 -0300

    Add postgresDB config to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,17 @@ export default defineNuxtConfig({
     public: {
       socketPort: 3002,
       url: 'http://localhost'
+    },
+    db: {
+      postgres: {
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: process.env.POSTGRES_PORT || 5432,
+        user: process.env.POSTGRES_USER || 'postgres',
+        password: process.env.POSTGRES_PASSWORD || 'password',
+        database: process.env.POSTGRES_DB || 'voting_app',
+      }
     }
+
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
